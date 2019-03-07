b39c3d51ebcbfa773382d886236addec5c913f30
Christian Jansen <jansen-christian@web.de>
2019-03-07 20:58:31 +0100

    added new efficient program

---
+++ A1/FinnChris-fibo_efficient.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+parser = ArgumentParser(
+    prog='Inefficient Fibonacci Algorithm',
+    formatter_class=RawDescriptionHelpFormatter,
+    epilog='''Programm zur Berechnung einer Fibonaccizahl unter Verwendung eines rekursiven Ansatzes.
+    \n Autor: Christian Jansen
+    '''
+)
+parser.add_argument('-n', '--number',type=int, required=True, help='Index of the requested Fibbonacci number.')
+parser.add_argument('--all', action='store_true', default=False, help="Return list of numbers until requested Fibonacci number.")
+
+
+DEBUG=False
+if DEBUG:
+    args = parser.parse_args(['-n','7', '--all'])
+    #args = parser.parse_args(['-h'])
+else:
+    args = parser.parse_args()
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        n_1 = 1 #letztes Ergebnis
+        n_2 = 0 #vorletztes Ergebnis
+        for i in range(n-1):
+            result = n_1 + n_2
+            n_2 = n_1
+            n_1 = result
+        return result
+
+if args.all:
+    n = args.number + 1
+    result_string = ""
+    for i in range(n):
+        result = fibonacci(i)
+        result_string += str(result)
+        if i != n - 1:
+            result_string += ","
+    print(result_string)
+else:
+    result = fibonacci(args.number)
+    print(result)
